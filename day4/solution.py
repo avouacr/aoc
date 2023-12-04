965f733ae24e1d42fa09a67345b52522ec5023b7
avouacr <avouacr@pm.me>
2023-12-04 14:15:02 +0000

    day4

--- day4/solution.py
+++ day4/solution.py
@@ -1,8 +1,8 @@
 import sys
+import re
 
 
-def compute_points(card):
-    # Splitting the data row into two parts
+def count_winning_numbers(card):
     parts = card.split(' | ')
 
     # Parsing the two lists of numbers
@@ -11,10 +11,14 @@ def compute_points(card):
 
     # Counting how many numbers from list2 are in list1
     common = set(list1) & set(list2)
-    if len(common) == 1:
-        points = 1
-    elif len(common) > 1:
-        points = 2**(len(common)-1)
+    return len(common)
+
+
+def compute_points(card):
+    # Splitting the data row into two parts
+    n_winning_numbers = count_winning_numbers(card)
+    if n_winning_numbers > 0:
+        points = 2**(n_winning_numbers - 1)
     else:
         points = 0
 
@@ -35,12 +39,49 @@ def main1(file):
     return ""
 
 
+def build_cards_mapping(cards_list):
+    mapping = {}
+    for card in cards_list:
+        card_split = card.split(': ')
+        card_id = int(card_split[0].split(' ')[1])
+        if card_id not in mapping:
+            mapping[card_id] = {
+                'winning_number': count_winning_numbers(card),
+                'counter': 1
+            }
+
+    return mapping
+
+
+def count_final_cards(cards_mapping):
+    mapping = cards_mapping.copy()
+    total_counter = 0
+    for current_id in list(mapping.keys()):
+        while mapping[current_id]['counter'] > 0:
+            winning_number = mapping[current_id]['winning_number']
+            for i in range(1, winning_number + 1):
+                next_id = current_id + i
+                if next_id in mapping:
+                    mapping[next_id]['counter'] += 1
+
+            # Decrement the counter of the current id
+            mapping[current_id]['counter'] -= 1
+            total_counter += 1
+
+    return total_counter
+
+
 def main2(file):
 
     with open(file, 'r') as file:
-        lines = file.read().splitlines()
+        cards_deck = file.read()
+        cards_deck = re.sub(r"\s\s+", " ", cards_deck)
+        cards_deck = cards_deck.splitlines()
 
-    return ""
+    cards_mapping = build_cards_mapping(cards_deck)
+    n_final_cards = count_final_cards(cards_mapping)
+
+    return n_final_cards
 
 
 if __name__ == "__main__":
