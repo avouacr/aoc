8c20e8b2f660d7cf0bc0d94ba2217a1175ef7c53
avouacr <avouacr@pm.me>
2023-12-06 00:06:52 +0000

    fuck

--- day5/solution.py
+++ day5/solution.py
@@ -7,21 +7,13 @@ def extract_mappings(file):
         text = file_in.read()
     text_mappings = text.split('\n\n')[1:]
 
-    maps = []
+    mappings = []
     for m in text_mappings:
         m_rows = [row.split(' ') for row in m.split('\n')[1:]]
         m_rows = [[int(item) for item in sublist] for sublist in m_rows]
-        maps.append(m_rows)
-
-    return maps
-
-
-def build_mappings(rows):
-    mappings = []
-
-    for row in rows:
-        dst_start, src_start, length = row
-        mappings.append((src_start, src_start + length, dst_start - src_start))
+        for row in m_rows:
+            dst_start, src_start, length = row
+            mappings.append((src_start, src_start + length, dst_start - src_start))
 
     return mappings
 
@@ -30,11 +22,12 @@ def get_mapped_value(mappings, src_value):
     for mapping in mappings:
         src_start, src_end, offset = mapping
         if src_start <= src_value < src_end:
-            return src_value + offset
+            src_value = src_value + offset
+
     return src_value
 
 
-def generate_seeds(file, part):
+def yeild_seeds1(file):
     with open(file, 'r') as file_in:
         text = file_in.read()
     text_parsed = text.split('\n\n')
@@ -42,32 +35,46 @@ def generate_seeds(file, part):
     seeds = text_parsed[0].split(': ')[1].split(' ')
     seeds = [int(item) for item in seeds]
 
-    if part == 1:
-        for seed in seeds:
+    for seed in seeds:
+        yield seed
+
+
+def yeild_seeds2(file, every=1):
+    with open(file, 'r') as file_in:
+        text = file_in.read()
+    text_parsed = text.split('\n\n')
+
+    seeds = text_parsed[0].split(': ')[1].split(' ')
+    seeds = [int(item) for item in seeds]
+
+    for i in range(0, len(seeds) - 1, 2):
+        for seed in range(seeds[i], seeds[i] + seeds[i+1], every):
             yield seed
-    if part == 2:
-        for i in range(0, len(seeds) - 1, 2):
-            seeds_expanded = list(range(seeds[i], seeds[i] + seeds[i+1]))
-            for seed in seeds_expanded:
-                yield seed
 
 
-def main(file, part, mode='min'):
+def main1(file):
+    mappings = extract_mappings(file)
+    min_location_number = float('inf')
+    for seed in yeild_seeds1(file):
+        mapped_value = get_mapped_value(mappings, seed)
+        min_location_number = min(min_location_number, mapped_value)
+
+    return min_location_number
+
+
+def main2(file, yield_every=1000):
     mappings_rows = extract_mappings(file)
-    locations = []
-    for seed in generate_seeds(file, part=part):
+
+    seed_to_loc_number = []
+    for seed in yeild_seeds2(file, every=yield_every):
         mapped_value = seed
         for rows in mappings_rows:
             mappings = build_mappings(rows)
             mapped_value = get_mapped_value(mappings, mapped_value)
-        locations.append(mapped_value)
+        seed_to_loc_number.append((seed, mapped_value))
 
-    if mode == 'all':
-        result = locations
-    elif mode == 'min':
-        result = min(locations)
+    return seed_to_loc_number
 
-    return result
 
 
 if __name__ == "__main__":
@@ -78,16 +85,15 @@ if __name__ == "__main__":
     if PART == "1":
 
         if MODE == "test":
-            assert main(file="calibration.txt", part=1, mode='all') == [82, 43, 86, 35]
-            assert main(file="calibration.txt", part=1) == 35
+            assert main1(file="calibration.txt") == 35
         elif MODE == "main":
-            sol_part1 = main(file="puzzle.txt", part=1)
+            sol_part1 = main1(file="puzzle.txt")
             print(sol_part1)
 
     elif PART == "2":
 
         if MODE == "test":
-            assert main(file="calibration.txt", part=2) == 46
+            assert main1(file="calibration.txt") == 46
         elif MODE == "main":
-            sol_part2 = main(file="puzzle.txt")
+            sol_part2 = main2(file="puzzle.txt")
             print(sol_part2)
