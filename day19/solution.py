1cf9eee32adfd6e7c0ff40bd9185bf842b71516a
avouacr <avouacr@pm.me>
2023-12-24 15:30:08 +0100

    day 19 part1

---
+++ day19/solution.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def main1(file):
+
+    with open(file, 'r') as file_in:
+        lines = file_in.read().splitlines()
+
+    return ""
+
+
+def main2(file):
+
+    with open(file, 'r') as file_in:
+        lines = file_in.read().splitlines()
+
+    return ""
+
+
+if __name__ == "__main__":
+
+    PART = sys.argv[1]
+    MODE = sys.argv[2]
+
+    if PART == "1":
+
+        if MODE == "test":
+            assert main1(file="calibration.txt") == 19114
+        elif MODE == "main":
+            sol_part1 = main1(file="puzzle.txt")
+            print(sol_part1)
+
+    elif PART == "2":
+
+        if MODE == "test":
+            assert main2(file="calibration.txt") == ""
+        elif MODE == "main":
+            sol_part2 = main2(file="puzzle.txt")
+            print(sol_part2)
